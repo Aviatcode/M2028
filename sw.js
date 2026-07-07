8187f8c5da7ceb1d6bb7dc99240a4013b368ad29
Aviatcode <avikshitworks@gmail.com>
2026-07-07 13:57:10 +0530

    Implement service worker with caching strategy

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'M2028-v1';
+const ASSETS = ['/', '/index.html', '/susurrus.mp3'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
+      e.request.destination === 'document'
+        ? caches.match('/index.html')
+        : new Response('', {status: 408})
+    ))
+  );
+});
